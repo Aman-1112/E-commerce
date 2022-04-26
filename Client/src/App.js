ee89992236e56df74d65bfef45b8af6407801959
Aman Raj <87664455+Aman-1112@users.noreply.github.com>
2022-04-26 19:26:09 +0530

    Added CardList and Cards

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import CardList from "./components/CardList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Router>
         <Navbar/>
+        <CardList/>
       </Router>
     </div>
   );
